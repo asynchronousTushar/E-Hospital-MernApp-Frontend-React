31a045f0406017589ac00c621810079b65512220
Tushar Biswas <tusarbiswas888@gmail.com>
2022-04-10 23:49:34 +0600

    login page design complete

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -1,10 +1,51 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import { Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 
-const LogIn = (props) => {
+const LogIn = () => {
     return (
         <React.Fragment >
             <Header />
+            <Form inline className="p-5 mt-5 col-6 bg-dark m-auto rounded" >
+                <FormGroup className="mb-2 me-sm-2 mb-sm-0 text-light">
+                    <Label
+                        className="me-sm-2 mb-2"
+                        for="exampleEmail"
+                    >
+                        Email
+                    </Label>
+                    <Input
+                        id="exampleEmail"
+                        name="email"
+                        placeholder="Enter Your Email Address."
+                        type="email"
+                    />
+                </FormGroup>
+                <FormGroup className="mb-2 me-sm-2 mb-sm-0 text-light">
+                    <Label
+                        className="me-sm-2 mt-3 mb-2"
+                        for="examplePassword"
+                    >
+                        Password
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+                        placeholder="Enter Password"
+                        type="password"
+                    />
+                </FormGroup>
+                <Button className="mt-4 px-5" color="primary" outline>
+                    Log In
+                </Button>
+            </Form>
+            <div>
+                <h6 className="text-center text-light my-3">Or</h6>
+                <Button className="d-block col-3 m-auto py-2" color="dark">
+                    <NavLink to="/signup" className="nav-link text-light">Sign Up</NavLink>
+                </Button>
+            </div>
         </React.Fragment>
     );
 }
